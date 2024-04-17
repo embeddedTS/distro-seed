bf26c371addcd4c776a2fcba34719064f57bc260
Lionel D. Hummel <lionel@users.noreply.github.com>
2024-04-17 12:57:06 -0700

    Add TS-7180 support (#75)
    * components: add ts7180-utils to support the TS-7180's tshwctl
    
    * configs: add TS-7180 Debian 12/Ubuntu 23.04 headless
    
    The TS-7180 has its own defconfigs mainly because its utils are not
    common with other platforms (i.e., `tshwctl`, `silabs`, etc.).
    
    Also, until the common gpsd/pps PR is pulled, these defconfigs at least ensure the necessary packages are installed. That PR may not be required. Debian 10 required a gpsd config file specific to the TS-7180. The gpsd config that provided PPS with what it needed to fire up on a TS-7180 with GPS:
    
         https://github.com/embeddedTS/distro-seed/blob/b9d73509bb2c388fe30ff6980af57697686e52f8/tasks/components/gps%2Bpps/files/gpsd
    
    In testing this PR with the headless Debian 12 defconfig and a 5.10 kernel with TS-7180 support, however, no additional config was needed in order for the GPS to be recognized at boot and wired up to PPS:
    
    ```
    root@ts7180:~# ppstest /dev/pps0
    trying PPS source "/dev/pps0"
    found PPS source "/dev/pps0"
    ok, found 1 source(s), now start fetching data...
    source 0 - assert 1713377663.000172762, sequence: 24301 - clear  0.000000000, sequence: 0
    source 0 - assert 1713377664.000186027, sequence: 24302 - clear  0.000000000, sequence: 0
    source 0 - assert 1713377664.997746610, sequence: 24303 - clear  0.000000000, sequence: 0
    source 0 - assert 1713377666.000173015, sequence: 24304 - clear  0.000000000, sequence: 0
    source 0 - assert 1713377667.000182988, sequence: 24305 - clear  0.000000000, sequence: 0
    source 0 - assert 1713377668.000194919, sequence: 24306 - clear  0.000000000, sequence: 0
    ^C
    root@ts7180:~# 
    ```

---
+++ tasks/components/ts7180-utils/fetch.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+
+SOURCE="$DS_WORK/components/ts7180-utils/"
+GITURL="https://github.com/embeddedTS/ts7180-utils.git"
+GITVERSION="v1.0.0"
+
+install -d "$SOURCE"
+
+common/host/fetch_git.sh "$GITURL" "$GITVERSION" "$SOURCE"
