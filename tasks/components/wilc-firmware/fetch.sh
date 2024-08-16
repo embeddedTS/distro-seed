39684d3067459e71f5ef8c508cc7b92fe942fa8b
Kris Bahnsen <kris@embeddedTS.com>
2024-08-16 18:49:18 +0000

    tasks: components: wilc-firmware: bump to 16.3
    Signed-off-by: Kris Bahnsen <kris@embeddedTS.com>

--- tasks/components/wilc-firmware/fetch.sh
+++ tasks/components/wilc-firmware/fetch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-URL="https://github.com/linux4wilc/firmware/archive/refs/tags/wilc_linux_16_1.tar.gz"
+URL="https://github.com/linux4wilc/firmware/archive/refs/tags/wilc_linux_16_3.tar.gz"
 PRJ_WORK="$DS_WORK/wilc-firmware/"
 
 install -d "$DS_OVERLAY/lib/firmware/mchp/"
@@ -8,6 +8,6 @@ install -d "$PRJ_WORK"
 
 common/host/fetch_tar.sh "$URL" "$PRJ_WORK"
 
-install -m 644 "${PRJ_WORK}/firmware-wilc_linux_16_1/wilc3000_ble_firmware.bin" "$DS_OVERLAY/lib/firmware/mchp/"
-install -m 644 "${PRJ_WORK}/firmware-wilc_linux_16_1/wilc3000_wifi_firmware.bin" "$DS_OVERLAY/lib/firmware/mchp/"
-install -m 644 "${PRJ_WORK}/firmware-wilc_linux_16_1/LICENSE.wilc_fw" "$DS_OVERLAY/lib/firmware/mchp/"
+install -m 644 "${PRJ_WORK}/firmware-wilc_linux_16_3/wilc3000_ble_firmware.bin" "$DS_OVERLAY/lib/firmware/mchp/"
+install -m 644 "${PRJ_WORK}/firmware-wilc_linux_16_3/wilc3000_wifi_firmware.bin" "$DS_OVERLAY/lib/firmware/mchp/"
+install -m 644 "${PRJ_WORK}/firmware-wilc_linux_16_3/LICENSE.wilc_fw" "$DS_OVERLAY/lib/firmware/mchp/"
