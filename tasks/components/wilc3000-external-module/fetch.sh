9127e87043d8b6dd2b14da73f30074ecdeab51a2
Mark Featherston <mark@embeddedts.com>
2024-08-20 17:15:38 +0000

    tasks: wilc3000-external-module: Add CONFIG_DS_MODULE_WILC3000_GIT_VERSION

--- tasks/components/wilc3000-external-module/fetch.sh
+++ tasks/components/wilc3000-external-module/fetch.sh
@@ -2,7 +2,6 @@
 
 SOURCE="$DS_WORK/components/wilc3000-external/"
 GITURL="https://github.com/embeddedTS/wilc3000-external-module.git"
-GITVERSION="linux4microchip-2021.10-1"
 
 install -d "$SOURCE"
-common/host/fetch_git.sh "$GITURL" "$GITVERSION" "$SOURCE"
+common/host/fetch_git.sh "$GITURL" "$CONFIG_DS_MODULE_WILC3000_GIT_VERSION" "$SOURCE"
