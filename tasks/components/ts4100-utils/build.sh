74b06bec7c893d2790152503f3853165229e9074
Kris Bahnsen <kris@embeddedTS.com>
2024-09-04 22:32:44 +0000

    tasks: compenents: ts4100: build and install ZPU binaries
    Signed-off-by: Kris Bahnsen <kris@embeddedTS.com>

--- tasks/components/ts4100-utils/build.sh
+++ tasks/components/ts4100-utils/build.sh
@@ -1,9 +1,17 @@
 #!/bin/bash -e
 
-SOURCE="$DS_WORK/components/ts4100-utils/"
+SOURCE="${DS_WORK}/components/ts4100-utils/"
 
-cd "$SOURCE"
+cd "${SOURCE}"
 
 ./autogen.sh
 ./configure --host="$AUTOTOOLS_HOST" --prefix="${DS_OVERLAY}/usr/local/"
 make -j"$(nproc --all)" && make install
+
+cd "${SOURCE}/src/zpu"
+# TODO: The compiler name is a bit of a magic number that I don't know how
+# to best handle dynamically. For now, when updating release, this needs to
+# change too
+PATH=$PATH:"${DS_WORK}/x86_64-zpu-elf-gcc-3.4.2/bin/" make
+install -d "${DS_OVERLAY}/usr/local/bin/zpu"
+install -m 0644 *.bin "${DS_OVERLAY}/usr/local/bin/zpu/"
