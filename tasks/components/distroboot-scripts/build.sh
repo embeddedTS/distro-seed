43919eb13138e02c4ff4d8a58c967a80b8a94bb1
Lionel D. Hummel <lionel@embeddedTS.com>
2024-09-09 16:26:09 -0700

    tasks/components/distroboot-scripts: make arch-specific boot.source
    arm64 needs a slightly different boot.scr due to having a different
    kernel name and boot command than arm.
    
    To maintain commonality and deal with these slight differences,
    `boot.source` will be generated rather than stored.

--- tasks/components/distroboot-scripts/build.sh
+++ tasks/components/distroboot-scripts/build.sh
@@ -1,9 +1,36 @@
-#!/bin/bash -e
+#!/bin/bash
 
-install -d "$DS_OVERLAY/boot/"
+set -e
+
+if [ "${DS_TARGET_ARCH}" = "arm64" ]  ; then
+    KERNEL_FILE=Image
+    BOOT_CMD=booti
+else
+    KERNEL_FILE=zImage
+    BOOT_CMD=bootz
+fi
+#TMP_BOOT_SOURCE="${DS_WORK}/boot/boot.source"
+TMP_BOOT_SOURCE="boot.source"
+
+cat > "${TMP_BOOT_SOURCE}" <<EOF
+# mkimage -A arm -T script -C none -n 'boot' -d boot.source boot.scr
+
+setenv bootargs "console=\${console} rootwait init=/sbin/init loglevel=4"
+
+part uuid \${devtype} \${devnum}:\${distro_bootpart} bootuuid
+if test -n "\${bootuuid}"; then
+  setenv bootargs "\${bootargs} root=PARTUUID=\${bootuuid}"
+fi
+
+load \${devtype} \${devnum}:\${distro_bootpart} \${kernel_addr_r} \${prefix}${KERNEL_FILE} \
+&& load \${devtype} \${devnum}:\${distro_bootpart} \${fdt_addr_r} \${prefix}\${fdtfile} \
+&& echo "Booting \$DISTRO \$RELEASE from \${devtype} \${devnum}:\${distro_bootpart}..." \
+&& ${BOOT_CMD} \${kernel_addr_r} - \${fdt_addr_r}
+EOF
+
 mkimage -A arm -T script -C none -n 'boot' \
-        -d "${DS_TASK_PATH}/files/boot.source" "${DS_OVERLAY}/boot/boot.scr"
+        -d "${TMP_BOOT_SOURCE}" "${DS_OVERLAY}/boot/boot.scr"
 
 if [[ "${CONFIG_DS_COMPONENT_DISTROBOOT_SCRIPTS_INSTALL_SOURCE}" == 'y' ]]; then
-    install --target-directory="$DS_OVERLAY/boot/" "${DS_TASK_PATH}/files/boot.source"
+    install --target-directory="$DS_OVERLAY/boot/" "${TMP_BOOT_SOURCE}"
 fi
