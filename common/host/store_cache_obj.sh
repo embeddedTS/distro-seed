624282be7cf7a5eb008e82556ec9ba5bed2e44e7
Mark Featherston <mark@embeddedts.com>
2025-09-07 05:00:04 +0000

    Preserve xattrs

--- common/host/store_cache_obj.sh
+++ common/host/store_cache_obj.sh
@@ -7,4 +7,4 @@ CACHE_FILE="${DS_CACHE}/${KEY}.tar.lz4"
 
 install -d "$DS_CACHE"
 cd "$TARGET"
-tar -c -I"lz4" --numeric-owner -f "$CACHE_FILE" .
+tar --xattrs --acls -c -I"lz4" --numeric-owner -f "$CACHE_FILE" .
