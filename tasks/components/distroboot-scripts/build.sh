5fe35ca18ff8b7a387ebf46e161a3000524aa722
Mark Featherston <mark@embeddedts.com>
2025-09-29 19:24:39 +0000

    distroboot-scripts: Add DS_COMPONENT_DISTROBOOT_EXTENSION

--- tasks/components/distroboot-scripts/build.sh
+++ tasks/components/distroboot-scripts/build.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+SOURCE="${DS_WORK}/components/distroboot-scripts/"
+install -d "${SOURCE}"
+install -d "${DS_OVERLAY}/boot/"
+
 if [ "${DS_TARGET_ARCH}" = "arm64" ]  ; then
     KERNEL_FILE=Image
     BOOT_CMD=booti
@@ -9,9 +13,28 @@ else
     KERNEL_FILE=zImage
     BOOT_CMD=bootz
 fi
-TMP_BOOT_SOURCE="${DS_TASK_PATH}/boot/boot.source"
-[ ! -d "$(dirname ${TMP_BOOT_SOURCE})" ] && mkdir -p "$(dirname ${TMP_BOOT_SOURCE})"
+TMP_BOOT_SOURCE="${SOURCE}/boot.source"
 
+if [[ "${CONFIG_DS_COMPONENT_DISTROBOOT_EXTENSION}" == 'y' ]]; then
+cat > "${TMP_BOOT_SOURCE}" <<EOF
+# mkimage -A arm -T script -C none -n 'boot' -d boot.source boot.scr
+
+setenv bootargs "console=\${console} rootwait init=/sbin/init loglevel=4"
+setenv extension_overlay_cmd 'load \${devtype} \${devnum}:\${distro_bootpart} \${fdt_addr_r} \${prefix}\${extension_overlay_name}'
+
+part uuid \${devtype} \${devnum}:\${distro_bootpart} bootuuid
+if test -n "\${bootuuid}"; then
+  setenv bootargs "\${bootargs} root=PARTUUID=\${bootuuid}"
+fi
+
+load \${devtype} \${devnum}:\${distro_bootpart} \${kernel_addr_r} \${prefix}${KERNEL_FILE}
+load \${devtype} \${devnum}:\${distro_bootpart} \${fdt_addr_r} \${prefix}\${fdtfile}
+extension scan
+extension apply
+echo "Booting \$DISTRO \$RELEASE from \${devtype} \${devnum}:\${distro_bootpart}..."
+${BOOT_CMD} \${kernel_addr_r} - \${fdt_addr_r}
+EOF
+else
 cat > "${TMP_BOOT_SOURCE}" <<EOF
 # mkimage -A arm -T script -C none -n 'boot' -d boot.source boot.scr
 
@@ -22,16 +45,16 @@ if test -n "\${bootuuid}"; then
   setenv bootargs "\${bootargs} root=PARTUUID=\${bootuuid}"
 fi
 
-load \${devtype} \${devnum}:\${distro_bootpart} \${kernel_addr_r} \${prefix}${KERNEL_FILE} \
-&& load \${devtype} \${devnum}:\${distro_bootpart} \${fdt_addr_r} \${prefix}\${fdtfile} \
-&& echo "Booting \$DISTRO \$RELEASE from \${devtype} \${devnum}:\${distro_bootpart}..." \
-&& ${BOOT_CMD} \${kernel_addr_r} - \${fdt_addr_r}
+load \${devtype} \${devnum}:\${distro_bootpart} \${kernel_addr_r} \${prefix}${KERNEL_FILE}
+load \${devtype} \${devnum}:\${distro_bootpart} \${fdt_addr_r} \${prefix}\${fdtfile}
+echo "Booting \$DISTRO \$RELEASE from \${devtype} \${devnum}:\${distro_bootpart}..."
+${BOOT_CMD} \${kernel_addr_r} - \${fdt_addr_r}
 EOF
+fi
 
-[ ! -d "${DS_OVERLAY}/boot" ] && mkdir -p "${DS_OVERLAY}/boot"
 mkimage -A arm -T script -C none -n 'boot' \
-        -d "${TMP_BOOT_SOURCE}" "${DS_OVERLAY}/boot/boot.scr"
+    -d "${TMP_BOOT_SOURCE}" "${DS_OVERLAY}/boot/boot.scr"
 
 if [[ "${CONFIG_DS_COMPONENT_DISTROBOOT_SCRIPTS_INSTALL_SOURCE}" == 'y' ]]; then
-    install --target-directory="$DS_OVERLAY/boot/" "${TMP_BOOT_SOURCE}"
+    install --target-directory="${DS_OVERLAY}/boot/" "${TMP_BOOT_SOURCE}"
 fi
