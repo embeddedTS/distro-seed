0648a6c825f31caf6db144d00042c046456f686f
Kris Bahnsen <kris@embeddedTS.com>
2026-01-08 00:28:08 +0000

    components/ts4900-utils: bump to v3.0.0 for newer releases
    Debian 13 now uses libgpiod 2.x, the utils sources were updated
    to support this. Debian 12 still uses 1.6, so the older tag is
    still used for this for now.
    
    Signed-off-by: Kris Bahnsen <kris@embeddedTS.com>

--- tasks/components/ts4900-utils/fetch.sh
+++ tasks/components/ts4900-utils/fetch.sh
@@ -2,7 +2,14 @@
 
 SOURCE="$DS_WORK/components/ts4900-utils/"
 GITURL="https://github.com/embeddedTS/ts4900-utils.git"
-GITVERSION="v2.0.2"
+GITVERSION="v3.0.0"
+
+# Older distributions use libgpiod 1.x, we need to use an older release tag
+# of utils for that.
+if ( [ "${DS_DISTRO}" == "debian" ] && [ "${DS_RELEASE_NUM}" == "12" ] ) || \
+   ( [ "${DS_DISTRO}" == "ubuntu" ] && [ "${DS_RELEASE_NUM}" == "24.04" ] ); then
+	GITVERSION="v2.0.2"
+fi
 
 install -d "$SOURCE"
 
