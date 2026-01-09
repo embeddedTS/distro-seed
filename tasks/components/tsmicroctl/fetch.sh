6320d11b2fbd33e93b84cae94abf5780ab835ea4
Kris Bahnsen <kris@embeddedTS.com>
2026-01-09 19:42:05 +0000

    components/tsmicroctl: bump to v2.0.0 for newer releases
    Debian 13 now uses libgpiod 2.x, the utils sources were updated
    to support this. Debian 12 still uses 1.6, so the older tag is
    still used for this for now.
    
    Signed-off-by: Kris Bahnsen <kris@embeddedTS.com>

--- tasks/components/tsmicroctl/fetch.sh
+++ tasks/components/tsmicroctl/fetch.sh
@@ -2,7 +2,13 @@
 
 SOURCE="$DS_WORK/components/tsmicroctl/"
 GITURL="https://github.com/embeddedTS/tsmicroctl.git"
-GITVERSION="v1.0.3"
+GITVERSION="v2.0.0"
+
+# Older distributions use libgpiod 1.x, we need to use an older release tag
+# of utils for that.
+if [ "${DS_DISTRO}" == "debian" ] && [ "${DS_RELEASE_NUM}" == "12" ]; then
+	GITVERSION="v1.0.3"
+fi
 
 install -d "$SOURCE"
 
