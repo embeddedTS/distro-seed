79444c0e86a8207fd9816461e350f315482036fd
Kris Bahnsen <kris@embeddedTS.com>
2026-01-26 22:00:23 +0000

    components/ts7670-utils: bump to v3.0.0
    Debian 13 now uses libgpiod 2.x, the utils sources were updated
    to support this. Debian 12 still uses 1.6, so the older tag is
    still used for this for now.
    
    Signed-off-by: Kris Bahnsen <kris@embeddedTS.com>

--- tasks/components/ts7670-utils/fetch.sh
+++ tasks/components/ts7670-utils/fetch.sh
@@ -2,7 +2,13 @@
 
 SOURCE="$DS_WORK/components/ts7670-utils/"
 GITURL="https://github.com/embeddedTS/ts7670-utils-linux4.x.git"
-GITVERSION="v2.0.0"
+GITVERSION="v3.0.0"
+
+# Older distributions use libgpiod 1.x, we need to use an older release tag
+# of utils for that.
+if [ "${DS_DISTRO}" == "debian" ] && [ "${DS_RELEASE_NUM}" == "12" ]; then
+	GITVERSION="v2.0.0"
+fi
 
 install -d "$SOURCE"
 
