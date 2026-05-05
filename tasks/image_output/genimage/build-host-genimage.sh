b3206ed60abccfc5af30d13ad7f1ffb1b2d28147
Mark Featherston <mark@embeddedts.com>
2026-05-05 16:38:32 +0000

    build: fix genimage vm support

--- tasks/image_output/genimage/build-host-genimage.sh
+++ tasks/image_output/genimage/build-host-genimage.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -e
 
 SOURCE="$DS_WORK/components/host-genimage/"
-CACHE_KEY=$(common/host/gen_cache_key.sh)
+CACHE_KEY=$(/src/common/host/gen_cache_key.sh)
 
-if ! common/host/fetch_cache_obj.sh "$CACHE_KEY" "$SOURCE"; then
+if ! /src/common/host/fetch_cache_obj.sh "$CACHE_KEY" "$SOURCE"; then
     (
         cd "$SOURCE"
         ./autogen.sh
         ./configure
         make -j"$(nproc --all)"
     )
-    common/host/store_cache_obj.sh "$CACHE_KEY" "$SOURCE"
+    /src/common/host/store_cache_obj.sh "$CACHE_KEY" "$SOURCE"
 fi
