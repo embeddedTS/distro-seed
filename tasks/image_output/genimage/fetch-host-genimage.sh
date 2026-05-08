4fcc7efdd588c9dc4fa774626cd99f73f1bfbc06
Mark Featherston <mark@embeddedts.com>
2026-05-08 00:31:55 +0000

    genimage: use vm package

--- tasks/image_output/genimage/fetch-host-genimage.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-SOURCE="$DS_WORK/components/host-genimage/"
-GITURL="https://github.com/pengutronix/genimage.git"
-GITVERSION="v16"
-
-install -d "$SOURCE"
-
-common/host/fetch_git.sh "$GITURL" "$GITVERSION" "$SOURCE"
