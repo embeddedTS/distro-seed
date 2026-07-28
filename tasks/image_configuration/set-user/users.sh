embeddedTS/distro-seed

commit d2b853e448dc33e79e689d11681139d439c12195

tasks: set-user: Add task to configure user, groups, and sudo

tasks/image_configuration/set-user/users.sh
@@ -1,0 +1,34 @@
+#!/bin/bash -e
+
+if [ "$CONFIG_DS_USER_ROOT_EN" = "y" ]; then
+    # Permits passwordless login to root
+    passwd --delete root
+else
+    usermod --shell /sbin/nologin root
+fi
+
+if [ -n "$CONFIG_DS_USER_ROOT_PASSWORD" ]; then
+    echo root:password | chpasswd
+fi
+
+if [ "$CONFIG_DS_USER" = "y" ]; then
+    # Warning, gecos is being replaced with --comment after Debian bookworm
+    adduser --gecos "" --disabled-password "$CONFIG_DS_USER_NAME"
+    echo "$CONFIG_DS_USER_NAME:$CONFIG_DS_USER_PASSWORD" | chpasswd
+
+    if [ -z "$CONFIG_DS_USER_PASSWORD" ]; then
+        passwd --delete "$CONFIG_DS_USER_NAME"
+    fi
+
+    if [ "$CONFIG_DS_USER_SUDO" = "y" ]; then
+        usermod -aG sudo "$CONFIG_DS_USER_NAME"
+    fi
+
+    if [ -n "$CONFIG_DS_USER_GROUPS" ]; then
+        # Trimming leading/trailing spaces
+        CONFIG_DS_USER_GROUPS=$(echo "$CONFIG_DS_USER_GROUPS" | xargs)
+        # Replacing spaces with commas
+        CONFIG_DS_USER_GROUPS=$(echo "$CONFIG_DS_USER_GROUPS" | tr ' ' ',')
+        usermod -G "$CONFIG_DS_USER_GROUPS" "$CONFIG_DS_USER_NAME"
+    fi
+fi

tasks/image_configuration/Kconfig
@@ -1,6 +1,7 @@
 menu "Image Configuration"
 
 comment "Required Settings"
+source "tasks/image_configuration/set-user/Kconfig"
 source "tasks/image_configuration/set-locales/Kconfig"
 source "tasks/image_configuration/set-hostname/Kconfig"
 source "tasks/image_configuration/set-timezone/Kconfig"

tasks/image_configuration/set-user/Kconfig
@@ -1,0 +1,55 @@
+# We always configure users
+config DS_SET_USERS
+    bool
+    default "y"
+
+config DS_USER_ROOT_EN
+    bool "Allow local login of root"
+    default "n"
+    help
+      This option, when enabled, allows the local login of the 'root'
+      user in the system. It's usually recommended for system administration
+      and should be used with caution due to the elevated privileges
+      of the 'root' user. If unsure, say N.
+
+config DS_USER_ROOT_PASSWORD
+    depends on DS_USER_ROOT_EN
+    string "default root password"
+    help
+      If 'Allow local login of root' is enabled, you can set a default password 
+      for the 'root' user.
+
+config DS_USER
+    bool "Create normal user"
+    help
+      Enabling this option will create a non-privileged, normal user on the system.
+      This is generally recommended for performing tasks that don't require elevated 
+      privileges.
+
+config DS_USER_NAME
+    string "username"
+    depends on DS_USER
+    default "user"
+    help
+      If 'Create normal user' is enabled, this option allows you to specify the 
+      username of the created user.
+
+config DS_USER_PASSWORD
+    string "default password"
+    depends on DS_USER
+    help
+      This option allows you to set a default password for the newly created user.
+
+config DS_USER_SUDO
+    bool "Enable sudo"
+    depends on DS_USER
+    default "y"
+    help
+      This option includes and enables sudo for the new user.
+
+config DS_USER_GROUPS
+    string "additional user groups"
+    depends on DS_USER
+    help
+      This option is used to specify additional user groups for the created user.
+      Groups should be space separated.

tasks/image_configuration/set-user/manifest.yaml
@@ -1,0 +1,8 @@
+config: DS_SET_USERS
+tasks:
+  - cmd: users.sh
+    cmd_type: target
+    description: Setting up users
+  - cmd: packages.sh
+    cmd_type: packagelist
+    description: Adding user management packages

tasks/image_configuration/set-user/packages.sh
@@ -1,0 +1,6 @@
+#!/bin/bash
+
+# Currently the sudo package is always sudo on any ubuntu/debian release
+if [ "$CONFIG_DS_USER_SUDO" = "y" ]; then
+        echo "sudo"
+fi

commit 9839bc23a6c2ce2aa77d90e92b6cc116d1a8457c

tasks: set-user: honor configured root password
DS_USER_ROOT_PASSWORD is exposed in menuconfig as the default root
password, but users.sh ignores its value and always supplies the literal
default "password" to chpasswd whenever the option is non-empty. As a
result, custom passwords selected during image configuration are not
applied.

Pass the configured value to chpasswd. Use printf with a fixed format so
password contents are not interpreted as options or escape sequences.

Fixes: d2b853e448dc ("tasks: set-user: Add task to configure user, groups, and sudo")
Signed-off-by: Robert Sandell <robert@capemeters.com>
Signed-off-by: Mark Featherston <mark@embeddedts.com>

tasks/image_configuration/set-user/users.sh
@@ -8,7 +8,7 @@ else
 fi
 
 if [ -n "$CONFIG_DS_USER_ROOT_PASSWORD" ]; then
-    echo root:password | chpasswd
+    printf 'root:%s\n' "$CONFIG_DS_USER_ROOT_PASSWORD" | chpasswd
 fi
 
 if [ "$CONFIG_DS_USER" = "y" ]; then